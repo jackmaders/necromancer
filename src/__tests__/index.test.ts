84b9dbad4d9a9beb2f6cace0cd9ec814bb9cc064
Jack Maders <jackwmaders@gmail.com>
2025-06-16 23:08:31 +0100

    style: :art: biome formatting

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,9 +1,9 @@
-import {  describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { greet } from "../index.ts";
 
 describe("index", () => {
 	it("should not throw an error", () => {
-        const greeting = greet("World");
-        expect(greeting).toBe("Hello, World!");
-    });
+		const greeting = greet("World");
+		expect(greeting).toBe("Hello, World!");
+	});
 });
