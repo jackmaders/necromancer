588d70841e464a3540f80a5863d91a4a90a19cb6
Jack William Maders <42032268+jackmaders@users.noreply.github.com>
2025-06-17 07:44:42 +0100

    feat: :hook: add pre-commit hooks (#5)
    * feat: :hook: add pre-commit hooks
    
    * fix: :bug: fix CI actions

---
+++ .commitlintrc.ts
@@ -0,0 +1,4 @@
+// biome-ignore lint/style/noDefaultExport: Default export is required for commitlint
+export default {
+	extends: ["@commitlint/config-conventional"],
+};
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
 import { defineConfig } from "vitest/config";
 
+// biome-ignore lint/style/noDefaultExport: Default export is required for vitest
 export default defineConfig({});
